83303b34ed9a68ceb0773d5c5b25fc5333d01a05
akhsan9 <maulanaakhsan9@gmail.com>
2020-10-13 10:18:49 +0700

    algoritma bubble_sort.py

---
+++ novice/01-01/bubble_sort.py
@@ -0,0 +1,11 @@
+def BubbleSort(val):
+    for passnum in range(len(val)-1,0,-1):
+        for i in range(passnum):
+            if val[i]>val[i+1]:
+                temp = val[i]
+                val[i] = val[i+1]
+                val[i+1] = temp
+ 
+DaftarAngka = [20,5,30,100,3,25,9,17]
+BubbleSort(DaftarAngka)
+print("BubbleSort",DaftarAngka)

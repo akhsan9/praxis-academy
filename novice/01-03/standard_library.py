d6077ad6f36f7a9dbbdc680c0df9bb65bbda6dbf
akhsan9 <maulanaakhsan9@gmail.com>
2020-10-15 16:17:20 +0700

    latihan library

---
+++ novice/01-03/standard_library.py
@@ -0,0 +1,6 @@
+import textwrap
+doc = """Metode wrap () sama seperti fill () kecuali metode ini mengembalikan
+daftar string, bukan satu string besar dengan baris baru untuk dipisahkan
+garis yang dibungkus."""
+
+print(textwrap.fill(doc, width=40))

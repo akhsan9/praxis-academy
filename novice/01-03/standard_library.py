d73e6e6e85d11d5e1079580e19561a073230b5ba
akhsan9 <maulanaakhsan9@gmail.com>
2020-10-19 13:51:09 +0700

    tambah kode hari 3

--- novice/01-03/standard_library.py
+++ novice/01-03/standard_library.py
@@ -3,4 +3,5 @@ doc = """Metode wrap () sama seperti fill () kecuali metode ini mengembalikan
 daftar string, bukan satu string besar dengan baris baru untuk dipisahkan
 garis yang dibungkus."""
 
+print('ini di file',(textwrap))
 print(textwrap.fill(doc, width=40))
